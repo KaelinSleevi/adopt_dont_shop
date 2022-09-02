4449dc388fbc943c9947166c5bbe5eaae6e5a039
Sandy Griffin <Sandra.Griffin@colorado.edu>
2022-09-01 21:41:29 -0600

    feat: updated schema and added controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_13_001717) do
+ActiveRecord::Schema.define(version: 2022_09_02_032717) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "applications", force: :cascade do |t|
+    t.string "name"
+    t.string "street_address"
+    t.string "city"
+    t.string "state"
+    t.string "zipcode"
+    t.string "status"
+  end
 
   create_table "pets", force: :cascade do |t|
     t.boolean "adoptable"
@@ -35,24 +44,5 @@ ActiveRecord::Schema.define(version: 2021_02_13_001717) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "veterinarians", force: :cascade do |t|
-    t.boolean "on_call"
-    t.integer "review_rating"
-    t.string "name"
-    t.bigint "veterinary_office_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["veterinary_office_id"], name: "index_veterinarians_on_veterinary_office_id"
-  end
-
-  create_table "veterinary_offices", force: :cascade do |t|
-    t.boolean "boarding_services"
-    t.integer "max_patient_capacity"
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   add_foreign_key "pets", "shelters"
-  add_foreign_key "veterinarians", "veterinary_offices"
 end
