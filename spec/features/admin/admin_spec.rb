7196acf4d9fad614aeb676a0397b308e8aad2ba0
Sandy Griffin <Sandra.Griffin@colorado.edu>
2022-09-05 20:26:37 -0600

    refactor: fixed tabbing and added end

--- spec/features/admin/admin_spec.rb
+++ spec/features/admin/admin_spec.rb
@@ -6,16 +6,12 @@ RSpec.describe 'admin shelters index' do
         @shelter2 = Shelter.create!(name: 'Denver shelter', city: 'Denver, CO', foster_program: false, rank: 5)
         @shelter3 = Shelter.create!(name: 'Thornton shelter', city: 'Thornton, CO', foster_program: false, rank: 3)
     end
-#     Admin Shelters Index
 
-# As a visitor
-# When I visit the admin shelter index ('/admin/shelters')
-# Then I see all Shelters in the system listed in reverse alphabetical order by name
-
-it 'can see all shelters in the system listed in reverse alphabetical order by name' do
-    visit '/admin/shelters'
-    expect(current_path).to eq('/admin/shelters')
-    expect(@shelter3.name).to appear_before(@shelter2.name)
-    expect(@shelter2.name).to appear_before(@shelter1.name)
-    expect(@shelter1.name).to_not appear_before(@shelter3.name)
+    it 'can see all shelters in the system listed in reverse alphabetical order by name' do
+        visit '/admin/shelters'
+        expect(current_path).to eq('/admin/shelters')
+        expect(@shelter3.name).to appear_before(@shelter2.name)
+        expect(@shelter2.name).to appear_before(@shelter1.name)
+        expect(@shelter1.name).to_not appear_before(@shelter3.name)
+    end
 end
